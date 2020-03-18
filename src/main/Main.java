161d1ddfe95a155a8ce45ee04868e542f3af6714
Daniel <daniel.tailor99@gmail.com>
2020-03-18 15:25:59 +0000

    binary tree remove preorder inorder postorder

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,15 +7,33 @@ public class Main {
     public static void main(String[] args) {
 
         TreeNode<Integer> root = new TreeNode<>(50, null, null);
+
         root.add(root,25);
         root.add(root,15);
-        root.add(root,40);
+        root.add(root,30);
         root.add(root,60);
+        root.add(root,55);
         root.add(root,75);
+        root.add(root,68);
+        root.add(root,80);
+        root.add(root,18);
+        root.add(root,58);
 
-        System.out.println(root.toString());
-        System.out.println(root.contains(root, 75));
-        System.out.println(root.contains(root,45));
+//        root.printPreOrder(root);
+//        System.out.println("");
+        System.out.println("Eredeti");
+        root.printInOrder(root);
+        root.remove(root, 50);
+        System.out.println("\nTorolt");
+        root.printInOrder(root);
+        System.out.println("");
+
+//        System.out.println("");
+//        root.printPostOrder(root);
+
+//        System.out.println(root.toString());
+//        System.out.println(root.contains(root, 75));
+//        System.out.println(root.contains(root,45));
 
 //        Node<String> head = new Node("Hetfo", null);
 //        head.addTail(head, "Kedd");
--- src/main/TreeNode.java
+++ src/main/TreeNode.java
@@ -10,11 +10,79 @@ public class TreeNode<T extends Comparable> {
         this.right = right;
     }
 
+    public TreeNode<T> remove(TreeNode<T> root, T value){
+        if(root == null) return null;
+        if(root.value.compareTo(value) == 1) root.left = remove(root.left, value);
+        else if(root.value.compareTo(value) == -1) root.right = remove(root.right, value);
+        else {
+            if(!isLeaf(root)) {
+                TreeNode<T> rightSmallest = findSmallest(root.right);
+                root.value = rightSmallest.value;
+                root.right = remove(root.right, rightSmallest.value);
+            } else if(root.left != null) root = root.left;
+            else if(root.right != null) root = root.right;
+            else root = null;
+        }
+        return root;
+    }
+
+    public boolean isLeaf(TreeNode<T> node){
+        return node.left == null && node.right == null;
+    }
+
+    public TreeNode<T> removeSmallest(TreeNode<T> root){
+        if(root == null) return null;
+        if (root.left == null)
+            return root;
+        else {
+            return findSmallest(root.left);
+        }
+    }
+
+    public TreeNode<T> findSmallest(TreeNode<T> root){
+        if(root == null) return null;
+        if (root.left == null)
+            return root;
+        else {
+            return findSmallest(root.left);
+        }
+    }
+
+    public TreeNode<T> findLargest(TreeNode<T> root){
+        if(root == null) return null;
+        if (root.right == null)
+            return root;
+        else {
+            return findSmallest(root.right);
+        }
+    }
+
+    public void printPreOrder(TreeNode<T> root){
+        if(root == null) return;
+        System.out.print(root.value + " "); // root
+        printPreOrder(root.left); // left
+        printPreOrder(root.right); // right
+    }
+
+    public void printInOrder(TreeNode<T> root){
+        if(root == null) return;
+        printInOrder(root.left); // left
+        System.out.print(root.value + " "); // root
+        printInOrder(root.right); // right
+    }
+
+    public void printPostOrder(TreeNode<T> root){
+        if(root == null) return;
+        printPostOrder(root.left); // left
+        printPostOrder(root.right); // right
+        System.out.print(root.value + " "); // root
+    }
+
     @Override
     public String toString() {
-        return " [value=" + value +
-                " left=" + left +
-                " right=" + right + "] ";
+        return " [value=" + value + //root
+                " left=" + left + //left
+                " right=" + right + "] "; //right
     }
 
     public boolean contains(TreeNode<T> root, T value){
